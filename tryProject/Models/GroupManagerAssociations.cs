21df97ed543c53073709c822e5fae7185ebfe885
Sveta-atevS <atevs.92@gmail.com>
2021-06-20 22:11:15 +0300

    ReUpload
    fingers crossed

---
+++ tryProject/Models/GroupManagerAssociations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tryProject.Models
+{
+    public class GroupManagerAssociations
+    {
+        public string GroupName { get; set; }
+
+        //public IEnumerable<Association> items { get; set; }
+    }
+}
